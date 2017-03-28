8c9a56a32eefea236832da59a2b018ca1c16e86e
Stanny Nuytkens <nuytkens.stanny@gmail.com>
2017-03-28 22:31:01 +0200

    Add @angular/material + hammerjs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,10 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 
+import { MaterialModule } from '@angular/material';
+
+import 'hammerjs';
+
 @NgModule({
   declarations: [
     AppComponent
@@ -12,7 +16,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
